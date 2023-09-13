3747877f60a280bdcc48e1cfe15564c554b13dd8
DaniBonica001 <danielabc_0106@hotmail.com>
2023-09-13 12:20:59 -0500

    virtual network

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "vm" {
 
 
 #Se crea la red
-resource "azurerm_virtual_network" "network" {
+resource "azurerm_virtual_network" "virtual_network" {
   name                = "${var.name_machine}-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.vm.location
@@ -26,7 +26,7 @@ resource "azurerm_virtual_network" "network" {
 resource "azurerm_subnet" "internal_subnet" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.vm.name
-  virtual_network_name = azurerm_virtual_network.network.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
